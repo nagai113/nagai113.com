effab2e6dea278a4999dca4374820898846c8f73
RYO NAGAI <seinaoasis@gmail.com>
2014-01-20 11:58:10 +0900

    画像の下の、コメント数等の記述を削除

--- single.php
+++ single.php
@@ -12,18 +12,6 @@
 			<div class="post-thumb">
 				<?php if(has_post_thumbnail()): the_post_thumbnail('single-post-image', array('class'=>'blog-thumb')); else: ?><?php endif; ?>
 
-				<div class="meta">
-					<span class="post-date"><?php echo get_the_date(M); ?> <?php echo get_the_date(d); ?></span>
-					<span class="post-comments">
-						<a href="<?php echo get_comments_link() ?>">
-							<?php $comments_count = get_comments_number( $post_id ); echo $comments_count; ?>
-						</a>
-						<em><?php _e('comments','site5framework') ?></em>
-					</span>
-				</div>
-
-				<span class="post-author"><em><?php _e('by','site5framework') ?></em> <?php the_author(); ?></span>
-
 			</div>
 			<!-- end .post-thumb -->
 
